a763189bd6638dfcb80b344c9b54546480b86795
Matteo Paonessa <matteo.paonessa@gmail.com>
2022-11-02 17:52:41 +0100

    Colors

--- scripts/chat-colors.js
+++ scripts/chat-colors.js
@@ -1,4 +1,20 @@
+const WHISPER_TYPE = 4;
+const EMOTE_TYPE = 3;
+const NORMAL_TYPE = 1;
+
 Hooks.on("renderChatMessage", (chatMessage, html, messageData) => {
     console.log(chatMessage, html, messageData);
-    html[0].classList.add('tpc-test');
+    const messageType = chatMessage.type;
+    let customClass = '';
+    switch (messageType) {
+        case WHISPER_TYPE:
+            customClass = 'tpc-whisper';
+            break;
+        case EMOTE_TYPE:
+            customClass = 'tpc-emote';
+            break;
+        default:
+
+    }
+    html[0].classList.add(customClass);
 });
